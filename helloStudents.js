2a1055b43828163d559c91188035753061c8abd0
Jad Jaber <jabe0043@algonquinlive.com>
2023-01-17 19:54:12 -0500

    Complete Exercise 1 - MAD 9124

--- helloStudents.js
+++ helloStudents.js
@@ -7,3 +7,24 @@
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+
+
+//Import and parse JSON file
+const students = require('./students.json');
+
+
+//Iterate through student array
+students.forEach((student) => console.log(`Hello ${student['firstName']} ${student['lastName']}`));
+
+//Number of last names starting with the letter D
+function countTheD(){
+    let arrD=[];
+    students.forEach((student) =>{
+        if (student['lastName'].startsWith('D')){
+            arrD.push(1);
+        }
+    })
+    let sum = arrD.reduce((a,b)=> a + b, 0);
+    console.log(`Count of last names starting with D is ${sum}`);
+}
+countTheD();
